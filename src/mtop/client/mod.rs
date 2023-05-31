bb78ae2d9cdc22e5310eb9032248a44697b94b85
Nick Pillitteri <nick@56quarters.xyz>
2023-05-30 20:20:12 -0400

    Support for `stats slabs` and `stats sizes` commands
    Add support to the client to get stats about slabs and item sizes.
    
    Part of #25

--- src/mtop/client/mod.rs
+++ src/mtop/client/mod.rs
@@ -1,5 +1,7 @@
 mod core;
 mod pool;
 
-pub use crate::client::core::{ErrorKind, Memcached, Meta, MtopError, ProtocolError, ProtocolErrorKind, Stats, Value};
+pub use crate::client::core::{
+    ErrorKind, Memcached, Meta, MtopError, ProtocolError, ProtocolErrorKind, Sizes, Slab, Slabs, Stats, Value,
+};
 pub use crate::client::pool::{MemcachedPool, PooledMemcached, TLSConfig};
